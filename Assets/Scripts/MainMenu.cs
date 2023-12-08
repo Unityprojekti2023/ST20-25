8bbc06faaffbee86f019cec86209770fb6bc8f78
JoniSetamaa <setamaajoni@gmail.com>
2023-12-08 11:57:32 +0200

    Muokattu päävalikko

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,13 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void OnPlayButton ()
+    public void OnStartButton ()
     {
+        Debug.Log("Start Game!");
         SceneManager.LoadScene(1);
     }
 
     public void OnQuitButton ()
     {
+        Debug.Log("Quit Game!");
         Application.Quit();
     }
 }
