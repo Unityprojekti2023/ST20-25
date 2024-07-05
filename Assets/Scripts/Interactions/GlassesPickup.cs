fa18d9e379bf5df9eaaa71805f8da11dfa1eb732
henmu <henri.mursu@gmail.com>
2024-07-05 08:39:06 +0300

    Reworked glasses pickup script
    Glasses script rework to be able to remove and equip glasses and simplified code

--- Assets/Scripts/Interactions/GlassesPickup.cs
+++ Assets/Scripts/Interactions/GlassesPickup.cs
@@ -3,18 +3,36 @@ using UnityEngine;
 
 public class GlassesPickup : MonoBehaviour, IInteractable
 {
-    public TextInformation textInfo;
     public GameObject glasses;
     public bool areGlassesEquipped = false;
     
     public void Interact()
     {
-        // Logic for handling Shoes interaction
-        InventoryManager.Instance.AddItemToInventory("Safety glasses");
-        textInfo.UpdateText("Safety glasses added");
-        ObjectiveManager.Instance.CompleteObjective("Put on safety glasses");
+        if (areGlassesEquipped)
+        {
+            RemoveGlasses();
+        }
+        else
+        {
+            EquipGlasses();
+        }
+    }
+
+    private void RemoveGlasses()
+    {
+        ObjectiveManager.Instance.CompleteObjective("Remove safety shoes");
+        // Update the text information
+        RayInteractor.instance.UpdateInteractionText(transform.name, "Put on glasses: [LMB] or [E]", InteractableType.HandleHoldInteraction);
+        areGlassesEquipped = false;
+        glasses.SetActive(true);
+    }
+
+    private void EquipGlasses()
+    {
+        ObjectiveManager.Instance.CompleteObjective("Put on safety shoes");
+        // Update the text information
+        RayInteractor.instance.UpdateInteractionText(transform.name, "Remove glasses: [LMB] or [E]", InteractableType.HandleHoldInteraction);
         areGlassesEquipped = true;
         glasses.SetActive(false);
-
     }
 }
